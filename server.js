fcbece413dba0e799881571d6cd17b3a2a75a419
jsaurabh01997 <jsaurabh01997@gmail.com>
2026-04-11 17:01:27 +0530

    update server.js

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const authRoutes = require("./routes/authRoutes"); // ✅ ADD THIS
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// ✅ CONNECT ROUTES
+app.use("/api/auth", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
@@ -19,5 +24,7 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-const PORT = 5000;
+// ⚠️ IMPORTANT (for Render)
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
